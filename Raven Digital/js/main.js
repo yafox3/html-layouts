b9c61f516b437fbabda95f99654a0f95a20637e8
yafox3 <ruslan.kret@yandex.ru>
2023-10-09 19:25:35 +0300

    made an adaptive

--- Raven Digital/js/main.js
+++ Raven Digital/js/main.js
@@ -7,17 +7,38 @@ $burger.onclick = () => {
 	document.body.classList.toggle('scrollable')
 }
 
-swiper = new Swiper('.slider', {
-	// direction: 'vertical',
+const swiper = new Swiper('.swiper', {
+	direction: 'vertical',
+	spaceBetween: 400,
 	slidesPerView: 1,
-	spaceBetween: 30,
 	mousewheel: true,
 	autoplay: {
         delay: 2500,
         disableOnInteraction: false,
     },
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-	}
+	breakpoints: {
+		0: {
+			direction: 'horizontal',
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			}
+		},
+		1200: {
+			direction: 'vertical',
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+				renderBullet: (index, className) => {
+					const sliders = document.querySelectorAll('.swiper-slide')
+					return `
+						${!index ? `<span class="start-slides">01</span>`: ''}
+						<span class="${className}"></span>
+						${Array.from(sliders).length === index + 1 ? `<span class="end-slides">0${sliders.length}</span>` : ``}
+					`
+				}
+			}
+		}
+	},
+	
 })
