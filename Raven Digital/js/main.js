65c3bcc6b0026c21d27599c824816ec33229de9d
yafox3 <ruslan.kret@yandex.ru>
2023-10-09 10:41:33 +0300

    made slider section (without adaptive)

--- Raven Digital/js/main.js
+++ Raven Digital/js/main.js
@@ -6,3 +6,18 @@ $burger.onclick = () => {
 	$nav.classList.toggle('active')
 	document.body.classList.toggle('scrollable')
 }
+
+swiper = new Swiper('.slider', {
+	// direction: 'vertical',
+	slidesPerView: 1,
+	spaceBetween: 30,
+	mousewheel: true,
+	autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	}
+})
