14021e4c557391105bbd747913c6eb171e9cc87b
yafox3 <ruslan.kret@yandex.ru>
2024-04-02 21:33:26 +0300

    fix: restyle

--- Dashboard/gulpfile.js
+++ Dashboard/gulpfile.js
@@ -1,91 +1,23 @@
-const { src, dest, watch, parallel, series }  = require('gulp');
+const gulp = require('gulp');
 
-const scss           = require('gulp-sass')(require('sass'));
-const concat         = require('gulp-concat');
-const browserSync   = require('browser-sync').create();
-const uglify         = require('gulp-uglify-es').default;
-const autoprefixer   = require('gulp-autoprefixer');
-const imagemin       = require('gulp-imagemin');
-const del             = require('del');
+// Tasks
+require('./gulp/dev.js');
+require('./gulp/docs.js');
 
-function browsersync() {
-  browserSync.init({
-    server : {
-      baseDir: 'src/'
-    }
-  });
-}
+gulp.task(
+	'default',
+	gulp.series(
+		'clean:dev',
+		gulp.parallel('html:dev', 'sass:dev', 'images:dev', gulp.series('svgStack:dev', 'svgSymbol:dev'), 'files:dev', 'js:dev'),
+		gulp.parallel('server:dev', 'watch:dev')
+	)
+);
 
-function cleanDist() {
-  return del('dist')
-}
-
-function images() {
-  return src('src/img/**/*')
-    .pipe(imagemin(
-      [
-        imagemin.gifsicle({ interlaced: true }),
-        imagemin.mozjpeg({ quality: 75, progressive: true }),
-        imagemin.optipng({ optimizationLevel: 5 }),
-        imagemin.svgo({
-          plugins: [
-            { removeViewBox: true },
-            { cleanupIDs: false }
-          ]
-        })
-      ]
-    ))
-    .pipe(dest('dist/img'))
-}
-
-function scripts() {
-  return src([
-    'src/js/main.js'
-  ])
-    .pipe(concat('main.min.js'))
-    .pipe(uglify())
-    .pipe(dest('src/js'))
-    .pipe(browserSync.stream())
-}
-
-
-function styles() {
-  return src('src/scss/style.scss')
-      .pipe(scss({outputStyle: 'compressed'}))
-      .pipe(concat('style.min.css'))
-      .pipe(autoprefixer({
-        overrideBrowserslist: ['last 10 version'],
-        grid: true
-      }))
-      .pipe(dest('src/css'))
-      .pipe(browserSync.stream())
-}
-
-function build() {
-  return src([
-    'src/css/style.min.css',
-    'src/fonts/**/*',
-    'src/js/main.min.js',
-    'src/*.html'
-  ], {base: 'src'})
-    .pipe(dest('dist'))
-}
-
-function watching() {
-  watch(['src/scss/**/*.scss'], styles);
-  watch(['src/js/**/*.js', '!src/js/main.min.js'], scripts);
-  watch(['src/*.html']).on('change', browserSync.reload);
-}
-
-exports.styles = styles;
-exports.watching = watching;
-exports.browsersync = browsersync;
-exports.scripts = scripts;
-exports.images = images;
-exports.cleanDist = cleanDist;
-
-
-exports.build = series(cleanDist, images, build);
-exports.default = parallel(styles ,scripts ,browsersync, watching);
-
-
+gulp.task(
+	'docs',
+	gulp.series(
+		'clean:docs',
+		gulp.parallel('html:docs', 'sass:docs', 'images:docs', gulp.series('svgStack:docs', 'svgSymbol:docs'), 'files:docs', 'js:docs'),
+		gulp.parallel('server:docs')
+	)
+);
